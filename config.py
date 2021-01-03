030e569e6ceea624cbc2ecab9fe1f2e67a9cb55a
Yoav Dobrin <37622785+yodobrin@users.noreply.github.com>
2021-01-03 16:42:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ settings = {
     'master_key': os.environ.get('ACCOUNT_KEY', '[YOUR KEY]'),
     'database_id': os.environ.get('COSMOS_DATABASE', '[YOUR DATABASE]'),
     'container_id': os.environ.get('COSMOS_CONTAINER', '[YOUR CONTAINER]'),
+    'MONGOURL': os.environ.get('MONGOURL', '[YOUR ENDPOINT]'),
+    'MONGO_USERNAME': os.environ.get('MONGO_USERNAME', '[YOUR USER NAME]'),
+    'MONGO_PASSWORD': os.environ.get('MONGO_PASSWORD', '[YOUR PASSWORD]')
 }
