d5f3260b593f976f63177ea2d9d7fa3664083c77
Yoav Dobrin <37622785+yodobrin@users.noreply.github.com>
2021-01-11 10:43:10 +0200

    Update keyvault_mid.py

--- keyvault_mid.py
+++ keyvault_mid.py
@@ -4,7 +4,7 @@ from azure.identity import DefaultAzureCredential
 import config
 
 keyVaultName = config.settings['kv_name']
-MASTER_KEY = config.settings['master_key']
+
 
 KVUri = 'https://'+keyVaultName+'.vault.azure.net'
 
@@ -14,4 +14,4 @@ client = SecretClient(vault_url=KVUri, credential=credential)
 secretName = config.settings['secret_name']
 
 retrieved_secret = client.get_secret(secretName)
-print(retrieved_secret)
+print(retrieved_secret.value)
